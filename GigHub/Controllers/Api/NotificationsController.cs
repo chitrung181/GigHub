935559c1cf2ee88dcea619dab81e04fe51ab43d0
Trung <chitrung181@gmail.com>
2020-03-18 15:00:57 +0700

    Not complete

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using GigHub.Core;
+using GigHub.Core.Dtos;
+using GigHub.Core.Models;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _unitOfWork.Notifications.GetNewNotificationsFor(userId);
+
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+        }
+
+        [HttpPost]
+        public IHttpActionResult MarkAsRead()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _unitOfWork.UserNotifications.GetUserNotificationsFor(userId);
+
+            notifications.ForEach(n => n.Read());
+
+            _unitOfWork.Complete();
+
+            return Ok();
+        }
+    }
+
+}
